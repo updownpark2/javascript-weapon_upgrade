12f32b6e3f800053f9687025d34cd3ed7a24a31e
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-12 17:33:54 +0900

    feat: 판단결과 강화 확률 증가 기능 추가

--- src/UpgradeGame.js
+++ src/UpgradeGame.js
@@ -1,15 +1,71 @@
+const generateMiniGameNumber = require(`./generateMiniGameNumber`);
+
 class UpgradeGame {
-  #upgrade = 0;
+  #upgradeCount = 0;
+  #upgradeProbability = 80;
 
   getUpgrade() {
-    return this.#upgrade;
+    return this.#upgradeCount;
   }
+  upgradePro;
 
   isChallenge(challenge) {
     if (challenge === `Y`) {
       return true;
     }
     return false;
+  }
+
+  #isNumber(miniGameInput) {
+    const numArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    if (numArr.includes(miniGameInput)) {
+      return true;
+    }
+  }
+  #isOddEven(miniGameInput) {
+    if (miniGameInput === "O" || miniGameInput === "E") {
+      return true;
+    }
+  }
+
+  plusProbability(miniGameInput) {
+    if (this.#isNumber(miniGameInput) === true) {
+      this.#judgementNumber(miniGameInput);
+    }
+    if (this.#isOddEven(miniGameInput) === true) {
+      this.#judgementOddEven(miniGameInput);
+    }
+  }
+
+  #judgementNumber(miniGameInput) {
+    const randomNum = generateMiniGameNumber();
+    if (miniGameInput === randomNum) {
+      this.#upgradeProbability = this.#upgradeProbability + 50;
+    }
+  }
+
+  #judgementOddEven(miniGameInput) {
+    const randomOddEvenNum = generateMiniGameNumber();
+    const miniGameInputNum = this.#translateToNum(miniGameInput);
+
+    if (randomOddEvenNum === miniGameInputNum) {
+      this.#upgradeProbability = this.#upgradeProbability + 10;
+    }
+  }
+
+  #translateToNum(miniGameInput) {
+    if (miniGameInput === "O") {
+      return 1;
+    }
+    return 0;
+  }
+
+  #upgradeCountUp() {
+    this.#upgradeCount = this.#upgradeCount + 1;
+  }
+
+  #resetProbability() {
+    this.#upgradeProbability = 90 - this.#upgradeCount * 10;
   }
 }
 

d66005b0f08af374a0f9959f2d834257ca304f20
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-12 14:28:30 +0900

    feat: 게임 시작 멘트 기능 구현

---
+++ src/Controller.js
@@ -0,0 +1,8 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+const InputView = require(`./InputView`);
+
+class Controller {
+  startMent() {
+    InputView.startMent();
+  }
+}
--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,10 @@
+const { Random, Console } = require("@woowacourse/mission-utils");
+
 const InputView = {
+  startMent() {
+    Console.print(`무기 강화 게임을 시작합니다.`);
+  },
+
   // 강화 도전 여부
   readChallengeCommand() {},
 

a1cef27b55f752a095bf92d14bb7035582bbd88d
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-12 15:33:08 +0900

    feat: 미니게임을 위한 Input 타당성 검사

--- src/Validation.js
+++ src/Validation.js
@@ -15,6 +15,44 @@ class Validation {
     this.#checkChallengeRange(challenge);
     this.#checkChallengeLength(challenge);
   }
+
+  #checkMinigameInputNumRange(miniGameInput) {
+    if (/^[0-9]*$/g.test(miniGameInput) === false) {
+      throw new Error(`[ERROR] 0~9사이의 숫자만 입력해주세요.`);
+    }
+  }
+
+  #checkMinigameInputNumLength(miniGameInput) {
+    if (miniGameInput.length !== 1) {
+      throw new Error(`[ERROR] 숫자는 1개만 입력이 가능합니다.`);
+    }
+  }
+
+  #checkMinigameInputOddEvenRange(miniGameInput) {
+    if (/^[O|X]/g.test(miniGameInput) === false) {
+      throw new Error(`[ERROR] O 또는 X 를 입력해주세요.`);
+    }
+  }
+
+  #checkMinigameInputOddEvenLength(miniGameInput) {
+    if (miniGameInput.lenth !== 1) {
+      throw new Error(`[ERROR] O 또는 X, 1개만 입력이 가능합니다.`);
+    }
+  }
+
+  #checkMinigameInputOddEven(miniGameInput) {
+    this.#checkMinigameInputOddEvenRange(miniGameInput);
+    this.#checkMinigameInputOddEvenLength(miniGameInput);
+  }
+
+  #checkMinigameInputNum(miniGameInput) {
+    this.#checkMinigameInputNumRange(miniGameInput);
+    this.#checkMinigameInputNumLength(miniGameInput);
+  }
+  checkMinigameInput(miniGameInput) {
+    this.#checkMinigameInputNum(miniGameInput);
+    this.#checkMinigameInputOddEven(miniGameInput);
+  }
 }
 
 module.exports = Validation;

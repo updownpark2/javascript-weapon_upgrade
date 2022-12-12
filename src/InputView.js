c2c2acc1a162b7b3b2709aa2f69703419a7ad83c
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-12 15:00:26 +0900

    feat: 강화 도전 여부 입력받는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -6,7 +6,12 @@ const InputView = {
   },
 
   // 강화 도전 여부
-  readChallengeCommand() {},
+  readChallengeCommand(callback) {
+    Console.readLine(
+      `강화 도전 여부를 입력해주세요.(도전: Y, 중단: N)`,
+      callback
+    );
+  },
 
   // 미니 게임 숫자 혹은 커맨드
   readMiniGameInput() {},
